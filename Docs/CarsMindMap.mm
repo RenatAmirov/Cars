4ba31f7aa2704cfd8b6844f89a10f67fbfe39a16
ramirov <renat.amirov@homecredit.ru>
2019-04-18 22:26:40 +0400

    Add Docs.

---
+++ Docs/CarsMindMap.mm
@@ -0,0 +1,133 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="TestProject" FOLDED="false" ID="ID_1858884182" CREATED="1554995294530" MODIFIED="1554995596887" LINK="Juri%20Cherepanov%20-%20Mulko.mm">
+<edge STYLE="bezier" COLOR="#ff0000" WIDTH="thin"/>
+<hook NAME="MapStyle" zoom="1.933" layout="OUTLINE">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="esntl-angular-for-asp.net-core-mvc-master.zip" POSITION="right" ID="ID_1629834426" CREATED="1554995381226" MODIFIED="1554995389100" LINK="esntl-angular-for-asp.net-core-mvc-master.zip">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="backend_cars_crud_assignment.pdf" POSITION="left" ID="ID_689545543" CREATED="1554995599770" MODIFIED="1554995609633" LINK="backend_cars_crud_assignment.pdf">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="backend_cars_crud_assignment.docx" POSITION="right" ID="ID_1819817710" CREATED="1554995639187" MODIFIED="1554995648188" LINK="backend_cars_crud_assignment.docx">
+<edge COLOR="#ff00ff"/>
+<node ID="ID_224792377" CREATED="1555004550850" MODIFIED="1555004607264"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#000000">&#1055;&#1088;&#1077;&#1076;&#1091;&#1087;&#1088;&#1077;&#1078;&#1076;&#1077;&#1085;&#1080;&#1077; NETSDK1059 </font>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <font color="#000000">&#1080;&#1085;&#1089;&#1090;&#1088;&#1091;&#1084;&#1077;&#1085;&#1090; &quot;</font><font color="#ff0000"><b>Microsoft.EntityFrameworkCore.Tools.DotNet&quot;</b></font><font color="#000000"><u>&#160; &#1090;&#1077;&#1087;&#1077;&#1088;&#1100; &#1074;&#1082;&#1083;&#1102;&#1095;&#1077;&#1085; &#1074; </u></font><u><font color="#0000c0"><b>&#1087;</b></font></u><font color="#0000c0"><b>&#1072;&#1082;&#1077;&#1090;&#160;SDK &#1076;&#1083;&#1103; .NET&#160;Core. </b></font>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <font color="#000000">&#1057;&#1074;&#1077;&#1076;&#1077;&#1085;&#1080;&#1103; &#1086; &#1090;&#1086;&#1084;, &#1082;&#1072;&#1082; &#1091;&#1089;&#1090;&#1088;&#1072;&#1085;&#1080;&#1090;&#1100; &#1101;&#1090;&#1086; &#1087;&#1088;&#1077;&#1076;&#1091;&#1087;&#1088;&#1077;&#1078;&#1076;&#1077;&#1085;&#1080;&#1077;:</font><font color="#008000"><b>&#160;https://aka.ms/dotnetclitools-in-box. </b></font>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <font color="#000000">TestProject C:\Program Files\dotnet\sdk\2.1.403\Sdks\Microsoft.NET.Sdk\targets\Microsoft.NET.ObsoleteReferences.targets 33 </font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<font SIZE="6"/>
+<node TEXT="&#x423;&#x434;&#x430;&#x43b;&#x438;&#x442;&#x435; &#x441;&#x441;&#x44b;&#x43b;&#x43a;&#x438; &lt;DotNetCliToolReference&gt;" ID="ID_1974586734" CREATED="1555004719496" MODIFIED="1555004721422">
+<node TEXT="&#x412;&#x447;&#x435;&#x440;&#x430; &#x43d;&#x430;&#x448;&#x435;&#x43b; &#x439;&#x43e;&#x43a;&#x441;&#x435;&#x43b;&#x44c;-&#x448;&#x430;&#x431;&#x43b;&#x43e;&#x43d;&#x447;&#x438;&#x43a; &#x434;&#x43b;&#x44f; Gantt-&#x434;&#x438;&#x430;&#x433;&#x440;&#x430;&#x43c;&#x43c; &#x438; &#x443;&#x43f;&#x440;&#x430;&#x432;&#x43b;&#x435;&#x43d;&#x438;&#x44f; &#x43f;&#x440;&#x43e;&#x435;&#x43a;&#x442;&#x438;&#x43a;&#x430;&#x43c;&#x438;.&#xa;&#x412; &#x43f;&#x440;&#x438;&#x43d;&#x446;&#x438;&#x43f;&#x435; &#x432; &#x431;&#x435;&#x441;&#x43f;&#x43b;&#x430;&#x442;&#x43d;&#x43e;&#x439; &#x435;&#x433;&#x43e; &#x447;&#x430;&#x441;&#x442;&#x438; &#x435;&#x441;&#x442;&#x44c; &#x441;&#x430;&#x43c;&#x43e;&#x435; &#x43d;&#x435;&#x43e;&#x431;&#x445;&#x43e;&#x434;&#x438;&#x43c;&#x43e;&#x435; &#x434;&#x43b;&#x44f; &#x438;&#x43d;&#x434;&#x438;-&#x43f;&#x440;&#x43e;&#x433;&#x440;&#x430;&#x43c;&#x43c;&#x438;&#x441;&#x442;&#x430;." ID="ID_1840961372" CREATED="1555047015635" MODIFIED="1555047017013"/>
+</node>
+</node>
+</node>
+</node>
+</map>
